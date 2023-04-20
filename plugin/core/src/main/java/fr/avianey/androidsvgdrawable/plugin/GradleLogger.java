7a43d0511f4ff26a49aaec1b78c294100fe67806
avianey <antoine.vianey@gmail.com>
2023-04-20 23:48:03 +0200

    Migrate to Gradle DSL Build working with Gradle + AGP 8.0
    1. Import as gradle project
    2. ./gradlew build
    3. import fr.avianey.androidsvgdrawable:core:8.0
    4. rewrite task type
    5. use dir as input 'from'
    
    task iconToMipmapPng(type: fr.avianey.androidsvgdrawable.plugin.SvgDrawableTask) {
        from = file('src/main/svg/mipmap')
        to = file('build/generated/res/main')
        createMissingDirectories = true
        outputType = 'mipmap'
        targetedDensities = ['ldpi', 'hdpi', 'mdpi', 'xhdpi', 'xxhdpi', 'xxxhdpi']
        outputFormat = 'PNG'
    }

---
+++ plugin/core/src/main/java/fr/avianey/androidsvgdrawable/plugin/GradleLogger.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2013, 2014, 2015 Antoine Vianey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.avianey.androidsvgdrawable.plugin;
+
+import org.gradle.api.logging.Logger;
+
+import fr.avianey.androidsvgdrawable.util.Log;
+
+public class GradleLogger implements Log {
+
+    public final Logger log;
+
+    public GradleLogger(Logger log) {
+        this.log = log;
+    }
+
+    @Override
+    public boolean isDebugEnabled() {
+        return log.isDebugEnabled();
+    }
+
+    @Override
+    public void debug(CharSequence content) {
+        log.debug(String.valueOf(content));
+    }
+
+    @Override
+    public void debug(CharSequence content, Throwable error) {
+        log.debug(String.valueOf(content), error);
+    }
+
+    @Override
+    public void debug(Throwable error) {
+        log.debug("", error);
+    }
+
+    @Override
+    public boolean isInfoEnabled() {
+        return log.isInfoEnabled();
+    }
+
+    @Override
+    public void info(CharSequence content) {
+        log.info(String.valueOf(content));
+    }
+
+    @Override
+    public void info(CharSequence content, Throwable error) {
+        log.info(String.valueOf(content), error);
+    }
+
+    @Override
+    public void info(Throwable error) {
+        log.info("", error);
+    }
+
+    @Override
+    public boolean isWarnEnabled() {
+        return log.isWarnEnabled();
+    }
+
+    @Override
+    public void warn(CharSequence content) {
+        log.warn(String.valueOf(content));
+    }
+
+    @Override
+    public void warn(CharSequence content, Throwable error) {
+        log.warn(String.valueOf(content), error);
+    }
+
+    @Override
+    public void warn(Throwable error) {
+        log.warn("", error);
+    }
+
+    @Override
+    public boolean isErrorEnabled() {
+        return log.isErrorEnabled();
+    }
+
+    @Override
+    public void error(CharSequence content) {
+        log.error(String.valueOf(content));
+    }
+
+    @Override
+    public void error(CharSequence content, Throwable error) {
+        log.error(String.valueOf(content), error);
+    }
+
+    @Override
+    public void error(Throwable error) {
+        log.error("", error);
+    }
+
+}
---
+++ plugin/core/src/main/java/fr/avianey/androidsvgdrawable/plugin/SvgDrawableTask.java
@@ -0,0 +1,188 @@
+/*
+ * Copyright 2013, 2014, 2015 Antoine Vianey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.avianey.androidsvgdrawable.plugin;
+
+import fr.avianey.androidsvgdrawable.*;
+import groovy.lang.Closure;
+import org.gradle.api.DefaultTask;
+import org.gradle.api.Task;
+import org.gradle.api.file.FileCollection;
+import org.gradle.api.tasks.*;
+
+import javax.annotation.Nullable;
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class SvgDrawableTask extends DefaultTask implements SvgDrawablePlugin.Parameters {
+
+    @InputDirectory
+    public File from;
+    @OutputDirectory
+    public File to;
+    @Input
+    public boolean createMissingDirectories = DEFAULT_CREATE_MISSING_DIRECTORIES;
+    @Input
+    public Density.Value[] targetedDensities;
+    @Input
+    @Optional
+    public Density.Value noDpiDensity;
+
+    // nine patch
+    @InputFile
+    @Optional
+    public File ninePatchConfig;
+
+    // masking
+    @InputFiles
+    @Optional
+    public FileCollection svgMaskFiles;
+    @InputFiles
+    @Optional
+    public FileCollection svgMaskResourceFiles;
+    @OutputDirectory
+    @Optional
+    public File svgMaskedSvgOutputDirectory;
+    @Input
+    public boolean useSameSvgOnlyOnceInMask;
+
+    // type
+    @Input
+    public OutputType outputType = DEFAULT_OUTPUT_TYPE;
+
+    // format
+    @Input
+    public OutputFormat outputFormat = DEFAULT_OUTPUT_FORMAT;
+    @Input
+    public int jpgQuality = DEFAULT_JPG_QUALITY;
+    @Input
+    public int jpgBackgroundColor = DEFAULT_JPG_BACKGROUND_COLOR;
+
+    // deprecated
+    @Input
+    @Nullable
+    public BoundsType svgBoundsType = DEFAULT_BOUNDS_TYPE;
+
+    @Override
+    public Task configure(Closure closure) {
+        Task task = null;
+        try {
+            task = super.configure(closure);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (svgMaskedSvgOutputDirectory == null) {
+            svgMaskedSvgOutputDirectory = new File(getProject().getBuildDir(), "generated-svg");
+        }
+        return task;
+    }
+
+    @TaskAction
+    public void transcode() {
+        SvgDrawablePlugin plugin = new SvgDrawablePlugin(this, new GradleLogger(getProject().getLogger()));
+        plugin.execute();
+    }
+
+    @Override
+    @InputFiles
+    public Iterable<File> getFiles() {
+        if (from == null) {
+            return Collections.emptyList();
+        }
+        File[] a = from.listFiles();
+        if (a == null) {
+            return Collections.emptyList();
+        }
+        return Arrays.asList(a);
+    }
+
+    public File getFrom() {
+        return from;
+    }
+
+    @Override
+    public File getTo() {
+        return to;
+    }
+
+    @Override
+    public boolean isCreateMissingDirectories() {
+        return createMissingDirectories;
+    }
+
+    @Override
+    public Density.Value[] getTargetedDensities() {
+        return targetedDensities;
+    }
+
+    @Nullable
+    @Override
+    public Density.Value getNoDpiDensity() {
+        return noDpiDensity;
+    }
+
+    @Override
+    public File getNinePatchConfig() {
+        return ninePatchConfig;
+    }
+
+    @Override
+    public Iterable<File> getSvgMaskFiles() {
+        return svgMaskFiles;
+    }
+
+    @Override
+    public Iterable<File> getSvgMaskResourceFiles() {
+        return svgMaskResourceFiles;
+    }
+
+    @Override
+    public File getSvgMaskedSvgOutputDirectory() {
+        return svgMaskedSvgOutputDirectory;
+    }
+
+    @Override
+    public boolean isUseSameSvgOnlyOnceInMask() {
+        return useSameSvgOnlyOnceInMask;
+    }
+
+    @Override
+    public OutputType getOutputType() {
+        return outputType;
+    }
+
+    @Override
+    public OutputFormat getOutputFormat() {
+        return outputFormat;
+    }
+
+    @Override
+    public int getJpgQuality() {
+        return jpgQuality;
+    }
+
+    @Override
+    public int getJpgBackgroundColor() {
+        return jpgBackgroundColor;
+    }
+
+    @Override
+    public BoundsType getSvgBoundsType() {
+        return svgBoundsType;
+    }
+
+
+}
